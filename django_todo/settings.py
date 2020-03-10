001d95e28463105b12468bb7a9cd9ea675d7402a
johnj974 <jjdug@hotmail.com>
2020-03-10 09:15:48 +0000

    updated allowed users

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = '#ii_6w1^ap%n=$2r+$z-8l+lkxp2fo99lv_&+g06-m#%=pd@x4'
 DEBUG = True
 
 ALLOWED_HOSTS = ['simpler-django-todo.herokuapp.com',
-                 'localhost']
+                 'simpler-django-todo.herokuapp.com']
 
 
 # Application definition
