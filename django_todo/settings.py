f18258c9652157615c07369dfe3efb704a667003
johnj974 <jjdug@hotmail.com>
2020-03-10 12:14:23 +0000

    path for env added to settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 import dj_database_url
 
+from os import path
+if path.exists("env.py"):
+    import env
+
 if os.environ.get('DEVELOPMENT'):
     development = True
 else:
@@ -26,7 +30,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY2')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
