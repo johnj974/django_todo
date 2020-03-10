7b30601e89879112ad4b463118c50151ad062a1d
johnj974 <jjdug@hotmail.com>
2020-03-10 12:03:53 +0000

    .gitignore file added

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '#ii_6w1^ap%n=$2r+$z-8l+lkxp2fo99lv_&+g06-m#%=pd@x4'
+SECRET_KEY = os.environ.get('SECRET_KEY2')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = development
